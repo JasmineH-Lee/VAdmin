4cca2e3b7c9ff5c5ccbce815e6b518738eda9bae
lijiahong <lijiahong@higohappy.com>
2018-12-10 20:18:55 +0800

    MOD: 增加测试路由

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,27 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/bread',
+      name: '面包',
+      component: () => import('@/test/bread')
+    },
+    {
+      path: '/table',
+      name: '表格',
+      component: () => import('@/test/table')
+    },
+    {
+      path: '/test',
+      name: '测试',
+      children: [
+        {
+          path: '/bread',
+          name: '面包',
+          component: () => import('@/test/bread')
+        }
+      ]
     }
   ]
 })
