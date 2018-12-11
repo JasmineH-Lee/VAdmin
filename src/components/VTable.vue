a8d482184a9020583b1d9915ccd6e31b6fe4f6c3
lijiahong <lijiahong@higohappy.com>
2018-12-11 11:32:17 +0800

    ADD: 表格下方增加分页

--- src/components/VTable.vue
+++ src/components/VTable.vue
@@ -1,52 +1,82 @@
 <template>
+  <div>
     <div>
-        <div>
-            <div class="table-wrap">
-                <div class="table-header" ref="tableHead">
-                    <table>
-                        <colgroup>
-                            <col style="width: 300px;">
-                            <col style="width: 300px;">
-                            <col style="width: 300px;">
-                            <col style="width: 300px;">
-                            <col style="width: 300px;">
-                            <col style="width: 300px;">
-                        </colgroup>
-                        <thead>
-                            <tr>
-                                <th
-                                v-for="(col, index) in tableOptions.columns"
-                                :key="index">{{ col.title }}</th>
-                            </tr>
-                        </thead>
-                    </table>
-                </div>
-                <div class="table-body" ref="tableBody">
-                    <table>
-                        <colgroup>
-                            <col style="width: 300px;">
-                            <col style="width: 300px;">
-                            <col style="width: 300px;">
-                            <col style="width: 300px;">
-                            <col style="width: 300px;">
-                            <col style="width: 300px;">
-                        </colgroup>
-                        <tbody>
-                            <tr v-for="data in tableData" :key="data.id">
-                                <slot :item="data">
-                                    <td
-                                      v-for="(col, index) in tableOptions.columns"
-                                      :key="index">
-                                        {{ data[col.field] }}
-                                    </td>
-                                </slot>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
+      <div class="table-wrap">
+        <div class="table-header" ref="tableHead">
+          <table>
+            <colgroup>
+              <col style="width: 300px;">
+              <col style="width: 300px;">
+              <col style="width: 300px;">
+              <col style="width: 300px;">
+              <col style="width: 300px;">
+              <col style="width: 300px;">
+            </colgroup>
+            <thead>
+              <tr>
+                <th
+                v-for="(col, index) in tableOptions.columns"
+                :key="index">{{ col.title }}</th>
+              </tr>
+            </thead>
+          </table>
         </div>
+        <div class="table-body" ref="tableBody">
+          <table>
+            <colgroup>
+              <col style="width: 300px;">
+              <col style="width: 300px;">
+              <col style="width: 300px;">
+              <col style="width: 300px;">
+              <col style="width: 300px;">
+              <col style="width: 300px;">
+            </colgroup>
+            <tbody>
+              <tr v-for="data in tableData" :key="data.id">
+                  <slot :item="data">
+                      <td
+                        v-for="(col, index) in tableOptions.columns"
+                        :key="index">
+                          {{ data[col.field] }}
+                      </td>
+                  </slot>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div class="table-footer">
+          <!-- 分页 -->
+          <div class="pagination-block">
+            <div class="refresh-btn">刷新按钮</div>
+            <div class="pager">
+              <ul class="pager-ul">
+                <li>上一页</li>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+                <li>4</li>
+                <li>5</li>
+                <li>下一页</li>
+              </ul>
+            </div>
+            <div class="page-index">
+              <span>第</span>
+              <input type="number" :value="p">
+              <span>页</span>
+            </div>
+            <div class="page-sizes">
+              <span>每页</span>
+              <select v-model="size">
+                <option v-for="(size, index) in sizes" :key="index">{{ size }}</option>
+              </select>
+              <span>条</span>
+            </div>
+            <div class="page-total">共{{ total }}条</div>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 <script>
 export default {
@@ -66,7 +96,10 @@ export default {
   },
   data () {
     return {
-
+      p: '1',
+      size: '30',
+      total: '32',
+      sizes: [20, 30, 50, 80, 100]
     }
   },
   mounted () {
@@ -97,5 +130,21 @@ table {
     background: 0 0;
     border: none;
     table-layout: fixed;
+}
+.pagination-block {
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: flex-end;
+  align-items: center;
+}
+.pager {
+  flex-grow: 3;
+}
+.pager-ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-flow: row nowrap;
 }
 </style>
